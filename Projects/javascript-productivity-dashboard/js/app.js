3123b3a5c545c8b9699df11291c0125108855f28
Mohammad Anas <ansarianas53665@gmail.com>
2026-07-15 23:20:19 +0530

    feat: implement task creation

--- Projects/javascript-productivity-dashboard/js/app.js
+++ Projects/javascript-productivity-dashboard/js/app.js
@@ -3,7 +3,23 @@ const taskInput = document.getElementById("taskInput")
 const taskForm = document.getElementById("taskForm")
 const taskList = document.getElementById("taskList")
 
-console.log(heading)
-console.log(taskInput)
-console.log(taskForm)
-console.log(taskList)
+taskForm.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    const task = taskInput.value.trim()
+
+    if(task === ""){
+        alert("Please enter a task")
+        return
+    }
+
+    const li = document.createElement("li")
+
+    li.textContent = task
+    taskList.appendChild(li)
+    taskInput.value = ""
+
+    const button = document.createElement("button")
+    button.textContent = "❌"
+    li.appendChild(button)
+})
