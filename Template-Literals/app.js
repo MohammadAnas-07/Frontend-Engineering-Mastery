2cc55e7edcc93c3b77ac87e70cb1bce3f5ca69f5
Mohammad Anas <ansarianas53665@gmail.com>
2026-07-15 13:14:59 +0530

    template-literals

---
+++ Template-Literals/app.js
@@ -0,0 +1,11 @@
+const person = {
+    name: "Anas",
+    language: "JavaScript",
+    library: "React"
+}
+const {name,language,library} = person
+
+console.log(`My name is ${name}.`)
+console.log(`I am learning ${language}.`)
+console.log(`I love ${library}.`)
+
